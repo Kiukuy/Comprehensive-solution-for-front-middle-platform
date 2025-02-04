700896a0d64f4a0943d5286ff8fb14d018f823fd
Kiukuy <1489834520@qq.com>
2025-02-04 10:21:11 +0800

    移动端navigation模块完成

---
+++ src/libs/popup/index.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { useScrollLock } from '@vueuse/core'
+import { watch } from 'vue'
+
+const visible = defineModel({ type: Boolean, required: true })
+
+// ---滚动锁定---
+const isLocked = useScrollLock(document.body)
+watch(
+  visible,
+  (val) => {
+    isLocked.value = val
+  },
+  {
+    immediate: true
+  }
+)
+</script>
+
+<template>
+  <div class="">
+    <!-- teleport -->
+    <teleport to="body">
+      <!-- 蒙版 -->
+      <transition name="fade">
+        <div
+          v-if="visible"
+          class="bg-zinc-900/80 fixed top-0 left-0 z-40 h-screen w-screen"
+          @click="visible = false"
+        ></div>
+      </transition>
+      <!-- 内容 -->
+      <transition name="popup-down-up">
+        <div
+          v-if="visible"
+          v-bind="$attrs"
+          class="fixed z-50 w-screen bg-white bottom-0"
+        >
+          <slot></slot>
+        </div>
+      </transition>
+    </teleport>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.3s;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.popup-down-up-enter-active,
+.popup-down-up-leave-active {
+  transition: all 0.3s;
+}
+</style>
