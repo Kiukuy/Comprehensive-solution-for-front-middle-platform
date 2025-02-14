719bcfb4e956acc8dda22f58739d407aae828590
Kiukuy <1489834520@qq.com>
2025-02-14 21:22:48 +0800

    构建长列表组件

--- src/views/main/components/list/index.vue
+++ src/views/main/components/list/index.vue
@@ -11,26 +11,63 @@ let query = {
   page: 1,
   size: 20
 }
+
+// 数据是否在加载中
+const isLoading = ref(false)
+// 数据是否全部加载
+const isFinished = ref(false)
+// 数据源
 const pexelsList = ref([])
+/**
+ * 加载数据方法
+ */
 const getPexelsData = async () => {
+  // 数据全部加载完成则 return
+  if (isFinished.value) {
+    return
+  }
+  // 完成第一次请求之后，后续请求让 page 自增
+  if (pexelsList.value.length) {
+    query.page += 1
+  }
+  // 触发接口请求
   const res = await getPexelsList(query)
-  pexelsList.value = res.list
+  // 初始请求清空数据源
+  if (query.page === 1) {
+    pexelsList.value = res.list
+  } else {
+    pexelsList.value.push(...res.list)
+  }
+  // 判断数据是否全部加载完成
+  if (pexelsList.value.length === res.total) {
+    isFinished.value = true
+  }
+  // 修改 loading 标记
+  isLoading.value = false
 }
-getPexelsData()
 </script>
 
 <template>
-  <m-waterfall
-    class="w-full px-1"
-    :data="pexelsList"
-    :nodeKey="'id'"
-    :column="isMobileTerminal ? 2 : 5"
-    :picturePreReading="false"
-  >
-    <template v-slot="{ item, width }">
-      <item-vue :data="item" :width="width">{{ item }}</item-vue>
-    </template>
-  </m-waterfall>
+  <div>
+    <!-- 列表处理 -->
+    <m-infinite-list
+      v-model="isLoading"
+      :isFinished="isFinished"
+      @onLoad="getPexelsData"
+    >
+      <m-waterfall
+        class="w-full px-1"
+        :data="pexelsList"
+        :nodeKey="'id'"
+        :column="isMobileTerminal ? 2 : 5"
+        :picturePreReading="false"
+      >
+        <template v-slot="{ item, width }">
+          <item-vue :data="item" :width="width">{{ item }}</item-vue>
+        </template>
+      </m-waterfall>
+    </m-infinite-list>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
