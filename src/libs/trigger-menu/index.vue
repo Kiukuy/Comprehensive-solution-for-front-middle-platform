20d7d5f7d31346b36bf528756f4cd9cadbb1dd56
Kiukuy <1489834520@qq.com>
2025-02-25 11:32:23 +0800

    构建 trigger-menu 和 trigger-menu-item 并使用

---
+++ src/libs/trigger-menu/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {} from 'vue'
+</script>
+
+<template>
+  <div
+    class="min-w-[180px] bg-white dark:bg-zinc-800 flex justify-between items-center rounded-full shadow px-2 py-1"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
