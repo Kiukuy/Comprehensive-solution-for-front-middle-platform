4fa4ee33aa343ecd2685fa5b0c099818e9181a1c
Kiukuy <1489834520@qq.com>
2025-02-26 23:20:47 +0800

    移动端支付样式

---
+++ src/views/member/components/payment/mobile-payment/index.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue'
+import discountsVue from '../discounts.vue'
+import mobilePaySelectVue from './mobile-pay-select.vue'
+
+const isOpenPopup = ref(false)
+const onConfirmClick = () => {
+  isOpenPopup.value = !isOpenPopup.value
+}
+</script>
+
+<template>
+  <div
+    class="fixed left-0 bottom-0 w-screen text-center bg-white dark:bg-zinc-800 xl:hidden"
+  >
+    <!-- 特惠提示 -->
+    <discounts-vue></discounts-vue>
+    <!-- 支付 -->
+    <div class="flex justify-between text-xs px-1 py-0.5">
+      <div class="text-left text-zinc-900 dark:text-zinc-200">
+        <p>
+          券后合计：<span class="text-red-600 text-[16px] font-sans font-medium"
+            >￥</span
+          >
+          <span class="text-red-600 text-[22px] font-sans font-medium">9</span>
+          <span class="text-red-600 block">优惠券：限时立减 ￥10</span>
+        </p>
+      </div>
+      <m-button class="w-[120px]" :isActiveAnim="false" @click="onConfirmClick"
+        >立即开通</m-button
+      >
+    </div>
+    <!-- TODO: popup -->
+    <m-popup v-model="isOpenPopup" class="rounded-t">
+      <mobile-pay-select-vue
+        @onItemClick="onConfirmClick"
+      ></mobile-pay-select-vue>
+    </m-popup>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/views/member/components/payment/mobile-payment/mobile-pay-select.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {} from 'vue'
+
+const emits = defineEmits(['onItemClick'])
+</script>
+
+<template>
+  <div class="py-2 h-[80vh] flex-col">
+    <h2 class="text-xl text-zinc-900 dark:text-zinc-200 font-bold mb-2 px-1">
+      选择支付方式
+    </h2>
+    <!-- 支付宝 -->
+    <div
+      class="flex items-center px-2 py-2 border-b border-b-zinc-200 dark:border-b-zinc-600 active:bg-zinc-200 dark:active:bg-zinc-900"
+      @click="$emit('onItemClick')"
+    >
+      <img src="@/assets/images/alipay.png" alt="" class="w-4 h-4" />
+      <p class="text-xl ml-1 text-zinc-800 dark:text-zinc-200">支付宝</p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
