5eb81e6d96b64cd3c9a45644ce0d70503d22ebea
Kiukuy <1489834520@qq.com>
2025-02-26 22:08:22 +0800

    修复弹出层 menu 点击报错

--- src/views/main/components/menu/index.vue
+++ src/views/main/components/menu/index.vue
@@ -14,7 +14,7 @@ defineEmits(['onItemClick'])
         v-for="(item, index) in $store.getters.categorys"
         :key="item.id"
         class="text-lg text-zinc-900 dark:text-zinc-300 px-1 py-1.5 duration-100 active:bg-zinc-100 dark:active:bg-zinc-900"
-        @click="$emit('onItemClick', index)"
+        @click="$emit('onItemClick', item)"
       >
         {{ item.name }}
       </li>
